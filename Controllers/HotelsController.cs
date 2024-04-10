9993645c02afe1b741ba2c3b0bafc31fbf442109
Leon Wiese <l.wiese@reply.de>
2024-04-10 09:43:29 +0200

    Added GetHotels

--- Controllers/HotelsController.cs
+++ Controllers/HotelsController.cs
@@ -1,6 +1,16 @@
+using booking_api.Database;
+using booking_api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace booking_api.Controllers;
 
-public class HotelsController
+[Route("/hotels")]
+public class HotelsController(AppDbContext db) : Controller
 {
-    
+    [HttpGet]
+    public async Task<List<Hotel>> GetHotels()
+    {
+        return await db.Hotels.ToListAsync();
+    }
 }
