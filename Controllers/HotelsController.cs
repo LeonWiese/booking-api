b027337961c7cf674123f607c799d2358bd8de32
Leon Wiese <l.wiese@reply.de>
2024-04-10 10:21:25 +0200

    Added GetHotel

--- Controllers/HotelsController.cs
+++ Controllers/HotelsController.cs
@@ -6,11 +6,26 @@ using Microsoft.EntityFrameworkCore;
 namespace booking_api.Controllers;
 
 [Route("/hotels")]
+[Produces("application/json")]
 public class HotelsController(AppDbContext db) : Controller
 {
     [HttpGet]
     public async Task<List<Hotel>> GetHotels()
     {
         return await db.Hotels.ToListAsync();
+    }
+
+    [HttpGet("{hotelId}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<Hotel>> GetHotel(Guid hotelId)
+    {
+        var hotel = await db.Hotels.FindAsync(hotelId);
+        if (hotel == null)
+        {
+            return NotFound();
+        }
+
+        return hotel;
     }
 }
